378ad470e26326810c2839c38833533d745ae75d
visualstudio <visualstudio@122-PC17>
2019-03-29 19:54:38 -0400

    first commit

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace helloworld
 {
@@ -10,6 +11,16 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Download the file");
+            Download();
+            Console.ReadLine();
+
+        }
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download Complete");
         }
     }
+
 }
